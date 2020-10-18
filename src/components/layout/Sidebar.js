e68bf813338f07618227c8f1301aa020f14ae5a6
goodlana <lahee13579@gmail.com>
2020-10-18 23:43:42 +0900

    7-basic-frame-of-sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
 
 export const Sidebar = () => {
   return (
-    <div>
-      
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar_generic">
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>
+            Inbox
+          </span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>
+            Today
+          </span>
+        </li>
+        <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>
+          Next 7 days
+        </span>
+        </li>
+      </ul>
+
+      <div className="sidebar__middle">
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+
+      <ul className="sidebar__projects">Projects</ul>
+      Add Project Component
     </div>
-  )
-}
+  );
+};
