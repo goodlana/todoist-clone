f7d355bfcf732c2338378b5fd573cb50fa2e46fc
goodlana <lahee13579@gmail.com>
2020-11-02 00:35:19 +0900

    9-add-firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = firebase.initializeApp({
+  apiKey:'',
+  autoDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messageSenderId: '',
+  apiId:'',
+})
+
+export { firebseConfig as firebase };
